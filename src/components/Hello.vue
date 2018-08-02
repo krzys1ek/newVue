0907202ecf44da4dd190da9e7e25dd109d349c43
krzys1ek <combat100@gmail.com>
2018-08-02 15:08:22 +0000

    vue

---
+++ src/components/Hello.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+<div class="kolor">
+  {{ message }}
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'BOOM'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+$color: red;
+.kolor {
+  color: $color;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,35 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+
+
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  data: {
+    show: false,
+    tag: 'p'
+  },
+  render(createdElement) {
+    return createdElement(
+      'button',
+       {
+         attrs: {
+           type: 'submin'
+         },
+         props: {
+           foo: 'bar'
+         },
+         domProps: {
+           innerHTML: 'Jakis text'
+         }
+    }
+  );
+  }
+});
